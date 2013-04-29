cce7dd5c101bf16d46e63ccc883ca3eed8d83af7
Eli Horne <elihorne@gmail.com>
2013-04-28 22:01:03 -0400

    adding analytics

--- function.js
+++ function.js
@@ -1,5 +1,16 @@
 function init() {
 
+  // google analytics
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-40501051-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
   // sidebar positioning utility
   function positionSidebar() {
     var windowScrollTop = $(window).scrollTop();
@@ -224,8 +235,11 @@ function init() {
 
   // add links to the nav for graphic design
   buildNav();
+
+  // open external links in new tabs/windows
   externalLinkHandler();
 
   // listen for lightbox
   lightboxControl();
+
 }
